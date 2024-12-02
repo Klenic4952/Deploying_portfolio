8c210bef73ccc7dc466f89b05e362bf9f38aa559
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-12-02 13:59:45 +0100

    Edit text about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -50,8 +50,9 @@ export default function About() {
           graag in voor mijn praktijk SMART Voedingsadvies. Daarnaast heb ik 24 tot 28 uur 
           per week beschikbaar om mijn hybride loopbaan invulling te geven. ICT is voor mij een
           nieuw werkveld waar ik met veel interesse kennis mee heb gemaakt. Momenteel onderzoek 
-          ik de mogelijkheid om WordPress-websites te gaan maken voor klanten. Maar ik zou ook passen 
-          op een ICT-gerelateerde Servicedesk of in de rol van Digicoach. 
+          ik de mogelijkheid om WordPress-websites te gaan maken voor klanten. Ik zou ook passen 
+          op een ICT-gerelateerde Servicedesk of in de rol van Digicoach. Ik heb een voorkeur
+          voor een werkomgeving met maatschappelijke en/of duurzame impact.
         </p>
         <p className=" mb-4 px-2 lg:px-3">
           Kort samengevat: je vindt in mij een combinatie van sociale en communicatieve vaardigheden
