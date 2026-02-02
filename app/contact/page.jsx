66f7f507faadadf9017daec50e91ff169381ad7f
NICOLIEN KLEIN <nicolienklein@live.nl>
2026-02-02 22:18:27 +0100

    Change color title mobile

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../components/contact-form";
 export default function Contact() {
   return (
     <div className="bg-[#DBAA78] p-8 pt-5 lg:p-10 rounded-xl text-[17px]">
-      <div className="sticky top-0 -mx-2 px-3 pt-5 pb-2 mb-4 bg-purplelight border-b-2 border-black md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+      <div className="sticky top-0 -mx-2 px-3 pt-5 pb-2 mb-4 bg-[#DBAA78] border-b-2 border-black md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="lg:sr-only">CONTACT</h2>
       </div>
       <p className="text-purpledark">
